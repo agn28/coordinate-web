b263bc05a1606f35fbb1faac73928e7eb052599b
Md Feroj Bepari <feroj21@gmail.com>
2020-03-24 12:16:44 +0600

    Bar chart using live data

--- src/components/utils/BarChart.vue
+++ src/components/utils/BarChart.vue
@@ -1,38 +1,38 @@
 <script>
-    import { Bar } from 'vue-chartjs'
+import { Bar } from "vue-chartjs";
 
-    export default {
-        extends: Bar,
-        props: ['stats'],
-        data: () => ({
-            chartdata: {
-                labels: [],
-                datasets: [
-                    {
-                        label: "Careplan Pending",
-                        backgroundColor: "#81D4FA",
-                        data: []
-                    }, {
-                        label: "Careplan Completed",
-                        backgroundColor: "#81C784",
-                        data: []
-                    }
-                ]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false
-            }
-        }),
-
-        mounted () {
-          for (let index in this.stats) {
-            this.chartdata.labels.push(index);
-            this.chartdata.datasets[0].data.push(this.stats[index]['pending']);
-            this.chartdata.datasets[1].data.push(this.stats[index]['completed'])
-          }
-            this.renderChart(this.chartdata, this.options);
-            console.log(this.stats);
+export default {
+  extends: Bar,
+  props: ["stats"],
+  data: () => ({
+    chartdata: {
+      labels: [],
+      datasets: [
+        {
+          label: "Careplan Pending",
+          backgroundColor: "#81D4FA",
+          data: []
+        },
+        {
+          label: "Careplan Completed",
+          backgroundColor: "#81C784",
+          data: []
         }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
     }
+  }),
+
+  mounted() {
+    for (let index in this.stats) {
+      this.chartdata.labels.push(index);
+      this.chartdata.datasets[0].data.push(this.stats[index]["pending"]);
+      this.chartdata.datasets[1].data.push(this.stats[index]["completed"]);
+    }
+    this.renderChart(this.chartdata, this.options);
+  }
+};
 </script>
