93f0ea38070e290e4c3b4c8610932a9a77e3aad4
Rasel Ahmed <rasel@augnitive.com>
2020-10-20 11:34:37 +0600

    Patient details new UI added

--- src/components/patients/Overview.vue
+++ src/components/patients/Overview.vue
@@ -195,68 +195,79 @@
                             </div>       
                         </div>
                         <div class="tab-pane fade p-3" id="three" role="tabpanel" aria-labelledby="three-tab">
+
+                            <div class="encounter-details">
+                              <div class="row">
+                                <div class="col-sm-8">
+                                  
+                                </div>
+                              </div>
+                            </div>
+
                             <div class="timeline-wrapper" v-if="encounters.length > 0">
+
                               <div class="timeline">
-                                <div class="timeline-item" v-for="(encounter, index) in encounters" :key="index">
-                                  <div class="icon-wrapper">
-                                    <div class="icon"></div>
-                                  </div>
-                                  <div class="triangle-box"></div>
-                                  <div class="timeline-data bg-white">
-                                    <div class="date">{{ getDate(encounter)}}</div>
-                                    <div class="title text-capitalize">Follow-up Encounter: {{ encounter.body.type }}</div>
-                                    <div class="doctor" v-if="users.length > 0">
-                                      <img
-                                        src="../../assets/images/avatar/dummy-man-570x570.png"
-                                        class="rounded-circle img-fluid"
-                                        width="30"
-                                        height="30"
-                                        alt
-                                      />
-                                      <span>{{ getUser(encounter) }}</span>
+
+                                <div class="row">
+                                  <div class="col-sm-4 timeline-item" v-for="(encounter, index) in encounters" :key="index">
+
+                                    <div class="timeline-data bg-white">
+                                        <div class="date">{{ getDate(encounter)}}</div>
+                                        <div class="title text-capitalize">Follow-up Encounter: {{ encounter.body.type }}</div>
+                                        <div class="doctor" v-if="users.length > 0">
+                                          <img
+                                            src="../../assets/images/avatar/dummy-man-570x570.png"
+                                            class="rounded-circle img-fluid"
+                                            width="30"
+                                            height="30"
+                                            alt
+                                          />
+                                          <span>{{ getUser(encounter) }}</span>
+                                        </div>
+                                        <div class="observation-icons">
+                                          <img
+                                            src="../../assets/images/body_measurements_.png"
+                                            class
+                                          />
+                                          <img
+                                            src="../../assets/images/blood_pressure_.png"
+                                            class
+                                          />
+                                          <img
+                                            src="../../assets/images/blood_test_.png"
+                                            class
+                                          />
+                                          <img
+                                            src="../../assets/images/medical_history_.png"
+                                            class
+                                          />
+                                        </div>
+
+                                        <!-- <div class="assessment-pills pb-2">
+                                                              
+                                                              <div class="pill-item RED border-RED">BMI</div>
+                                                              <div class="pill-item RED border-RED">BP</div>
+                                                              <div class="pill-item RED border-RED">CVD RISK</div>
+                                                              <div class="pill-item AMBER border-AMBER">Cholesterol</div>
+                                                              
+                                        </div>-->
+                                        <br />
+
+                                        <div class="float-right">
+                                          
+                                        </div>
+                                        <router-link
+                                          :to="{ name: 'observations', params: { patientId: patientId, encounterId: encounter.id }}"
+                                          tag="a"
+                                          class="view float-right"
+                                          >View Encounter Details <i class="fas fa-arrow-right"></i></router-link>
+
+                                        <!-- <a href="view"> View Encounter Details</a> -->
+                                      
                                     </div>
-                                    <div v-if="encounter.body.observations" class="observation-icons">
-                                      <img
-                                        v-if="isObservationAvailable(encounter, 'survey')"
-                                        src="../../assets/images/questionnaire.png"
-                                        class
-                                      />
-                                      <img
-                                        v-if="isObservationAvailable(encounter, 'body_measurement')"
-                                        src="../../assets/images/body_measurements.png"
-                                        class
-                                      />
-                                      <img
-                                        v-if="isObservationAvailable(encounter, 'blood_pressure')"
-                                        src="../../assets/images/blood_pressure_sm.png"
-                                        class
-                                      />
-                                      <img
-                                        v-if="isObservationAvailable(encounter, 'blood_test')"
-                                        src="../../assets/images/blood_test.png"
-                                        class
-                                      />
-                                    </div>
-
-                                    <!-- <div class="assessment-pills pb-2">
-                                                          
-                                                          <div class="pill-item RED border-RED">BMI</div>
-                                                          <div class="pill-item RED border-RED">BP</div>
-                                                          <div class="pill-item RED border-RED">CVD RISK</div>
-                                                          <div class="pill-item AMBER border-AMBER">Cholesterol</div>
-                                                          
-                                    </div>-->
-                                    <br />
-
-                                    <router-link
-                                      :to="{name: 'observations', params:{patientId: patientId, encounterId: encounter.id}}"
-                                      tag="a"
-                                      class="view"
-                                    >View Encounter Details</router-link>
-
-                                    <!-- <a href="view"> View Encounter Details</a> -->
                                   </div>
                                 </div>
+                                
                               </div>
                             </div>       
                         </div>
