a2e0b6319749a5acfbd562d967c7042178256922
imrasel <rasel@augnitive.com>
2021-09-09 15:35:16 +0600

    chcp removed from unassigned patient

--- src/components/users-management/worklist/UnassignedPatientList.vue
+++ src/components/users-management/worklist/UnassignedPatientList.vue
@@ -384,7 +384,7 @@ export default {
 
     async getUsers() {
       let loader = this.$loading.show();
-      await this.$http.get("/users?role=chw,chcp").then(
+      await this.$http.get("/users?role=chw").then(
         (response) => {
           if (response.status == 200) {
             this.users = response.data;
